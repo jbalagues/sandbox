f3b3d048ccc44ea83bba342b30d56541fe356af7
jbalagues <jaumebalague@gmail.com>
2019-08-20 11:21:30 +0200

    maqueta inicial
    maqueta inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import * as $ from "jquery";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = "CodeSandbox";
+export class AppComponent implements OnInit {
+  title = "Jaumne";
+
+  ngOnInit() {
+    // Menu-toggle button
+    $(document).ready(function() {
+      $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+      });
+    });
+
+    // Scrolling Effect
+    $(window).on("scroll", function() {
+      if ($(window).scrollTop()) {
+        $("nav").addClass("black");
+      } else {
+        $("nav").removeClass("black");
+      }
+    });
+  }
 }
