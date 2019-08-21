2a861232d938f86cbc6fa0d3eebe820cabe40218
jbalagues <jaumebalague@gmail.com>
2019-08-21 17:24:09 +0200

    Slot funcionando

--- src/app/servicios/aws.service.ts
+++ src/app/servicios/aws.service.ts
@@ -1,28 +1,98 @@
-import { Injectable } from "@angular/core";
+import { Injectable, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+
+var AWS = require("aws-sdk");
+
+AWS.config.region = "us-east-2"; // Region
+
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: "us-east-2:ecf243b5-aa65-4020-9e57-a1b6f2d5746d"
+});
+
+/// Prepare to call Lambda function
+var lambda = new AWS.Lambda({
+  region: "us-east-2",
+  apiVersion: "2015-03-31"
+});
 
 @Injectable()
-export class AwsService {
-  //these could be configured
-  oddAllowed: boolean = true;
-  evenAllowed: boolean = false;
-  isAuthorized: boolean;
+export class AwsService implements OnInit {
+  //
 
-  authorize(numberPlate): Promise<string> {
-    if (numberPlate % 2 == 0) {
-      this.isAuthorized = this.evenAllowed;
-    } else {
-      this.isAuthorized = this.oddAllowed;
-    }
-    if (this.isAuthorized) {
-      return Promise.resolve("allowed to drive in the city center");
-    } else {
-      return Promise.reject("NOT allowed to drive in the city center");
+  /*
+  arn:aws:lambda:us-east-2:718930781757:function:slotpull
+  {
+    "isWinner": false,
+    "leftWheelImage": {
+      "file": {
+        "S": "hart_k.png"
+      }
+    },
+    "middleWheelImage": {
+      "file": {
+        "S": "hart_a.png"
+      }
+    },
+    "rightWheelImage": {
+      "file": {
+        "S": "diam_k.png"
+      }
     }
   }
+  */
+  //lambda = null;
 
-  handler = async (event: any = {}): Promise<any> => {
-    console.log("Hello World!");
-    const response = JSON.stringify(event, null, 2);
-    return response;
+  /*constructor() {
+   
+    const creds = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: "us-east-2:ecf243b5-aa65-4020-9e57-a1b6f2d5746d"
+    });
+
+    this.AWS.config.update({
+      region: "us-east-2",
+      apiVersion: "2015-03-31",
+      credentials: creds
+    });
+
+    this.lambda = new this.AWS.Lambda({
+      region: "us-east-2",
+      apiVersion: "2015-03-31"
+    });
+    
+  }*/
+
+  ngOnInit() {}
+
+  pullParams = {
+    FunctionName: "arn:aws:lambda:us-east-2:718930781757:function:slotpull",
+    InvocationType: "RequestResponse",
+    LogType: "None"
   };
+
+  tirada() {
+    console.log("tirada");
+
+    const creds = new AWS.CognitoIdentityCredentials({
+      IdentityPoolId: "us-east-2:ecf243b5-aa65-4020-9e57-a1b6f2d5746d"
+    });
+
+    AWS.config.update({
+      region: "us-east-2",
+      apiVersion: "2015-03-31",
+      credentials: creds
+    });
+
+    lambda = new AWS.Lambda({
+      region: "us-east-2",
+      apiVersion: "2015-03-31"
+    });
+    return lambda
+      .invoke(this.pullParams, function(err, data) {
+        console.log("ret");
+        if (err) console.log(err, err.stack);
+        // an error occurred
+        else console.log(data); // successful response
+      })
+      .promise();
+  }
 }
