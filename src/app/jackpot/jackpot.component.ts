1aedba84f5121ceab16a8aaf799aaa3172c5b53a
jbalagues <jaumebalague@gmail.com>
2019-08-21 10:32:31 +0200

    Add slot images

--- src/app/jackpot/jackpot.component.ts
+++ src/app/jackpot/jackpot.component.ts
@@ -1,10 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-jackpot",
   templateUrl: "./jackpot.component.html",
   styleUrls: ["./jackpot.component.css"]
 })
-export class JackpotComponent {
+export class JackpotComponent implements OnInit {
   constructor() {}
+
+  ngOnInit() {}
+
+  pullHandle(e) {
+    console.log("pullHandle");
+  }
+
+  initiatePull(e) {
+    console.log("initiatePull");
+  }
 }
